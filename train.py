da42fd1fd7b5b513f4bfdf58852a5fec4e618346
YanniPuhlik <hottpoint@gmail.com>
2017-12-12 23:00:14 -0600

    Add files via upload

---
+++ train.py
@@ -0,0 +1,12 @@
+def Collatz(number):
+    if number%2==0:
+        result=number//2
+    elif number%2==1:
+        result=number*3+1
+    return result
+while True:
+    try:
+        number = int(input("Enter the number: "))
+        print(Collatz(number))
+    except ValueError:
+        print('This is not a number. Please enter the number')
